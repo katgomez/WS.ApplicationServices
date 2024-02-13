f4dcc2da4405fdc751dccd46841c26583b7c2d5b
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-13 22:23:09 +0100

    some changes

--- obj/Debug/net7.0/WS.ApplicationServices.AssemblyInfo.cs
+++ obj/Debug/net7.0/WS.ApplicationServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WS.ApplicationServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702db7ca979823edd805a9dcf79d9deec459a449")]
 [assembly: System.Reflection.AssemblyProductAttribute("WS.ApplicationServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WS.ApplicationServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
